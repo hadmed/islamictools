99d04bf3f1013cb837ebfe7b2be964c2f334c8fa
amrounix <amrounix@dd1318b7-078e-7aba-bdaa-8d7167dea51a>
2010-11-28 21:42:48 +0000

    version 0.1c

---
+++ trunk/IslamicTools/src/com/alpha/commun/CityDB.java
@@ -0,0 +1,86 @@
+package com.alpha.commun;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.util.Log;
+
+public class CityDB extends SQLiteOpenHelper
+{
+	private static final int DATABASE_VERSION = 2;
+	private static final String DATABASE_NAME = "IslamicTools";
+	private Context context;
+
+   
+	public CityDB(Context context, String name, CursorFactory factory, int version)
+	{
+		super(context, name, factory, version);
+		this.context = context;
+	}
+
+   public CityDB(Context context) {
+      super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		this.context = context;
+		//Log.d("sam", "appel create DB ");
+  }
+
+   
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		InputStream fis=null;
+		try
+		{
+			fis =  context.getAssets().open("sql/gps.sql");
+	  	   BufferedReader bread = new BufferedReader(new InputStreamReader(fis));
+		   String country="";
+		   int cpt = -1;
+		   int cptCity = 0;
+		   //db.beginTransaction();
+	  	   while (bread.ready()) {
+		        String s = bread.readLine().trim();	
+		        if (s!=null && s.startsWith("#"))
+		        {
+		      	  country = s.substring(1);
+		      	  cpt++;
+		      	  cptCity = 0;
+		      	  db.execSQL("insert into country values ("+cpt+",\""+country+"\",0);");
+		      	  Log.d("sam","~~~~ insert into country values ("+cpt+",\""+country+"\",0);" );
+
+		        } else
+		        if (s!=null && !s.equals("") )
+		        {
+		      	  s = s.replaceAll("@", ""+cpt).replaceAll("#", ""+cptCity);
+		      	  Log.d("sam", "query : "+s);
+		      	  cptCity++;
+		      	  db.execSQL(s);
+		        }
+		    }
+	  	   //db.close();
+		} catch (Exception ex)
+		{
+			Log.d("sam", "erreur : "+ex.getMessage());			
+		} finally{
+			try{
+				if (fis!=null)
+					fis.close();
+			}catch (Exception e) {
+				
+			}	
+				
+			}
+			//db.close();
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+	}
+
+}
--- trunk/IslamicTools/src/com/alpha/model/Settings.java
+++ trunk/IslamicTools/src/com/alpha/model/Settings.java
@@ -3,17 +3,22 @@ package com.alpha.model;
 
 import java.util.TimeZone;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import com.alpha.commun.CityDB;
+import com.alpha.commun.Param;
 import com.alpha.commun.utils;
 
 
 public class Settings {
-	private double lat;
-	private double lon;
-	private double alt;
+	private long lat;
+	private long lon;
+	private int alt;
 	private String location;
 //	private double speed=0;	
 	private int gmt;
@@ -22,7 +27,6 @@ public class Settings {
 	private int country;
 	private int city;
 
-	private static String PARAM_NAME = "islamicTools";
 	private float angle;
 	private int method;
 	private Context context;
@@ -38,18 +42,25 @@ public class Settings {
 	
 	private Settings(Context context) {
 		this.context = context;
-		SharedPreferences param = context.getSharedPreferences(PARAM_NAME, 0);
+		SharedPreferences param = context.getSharedPreferences(Param.NAME, 0);
+
+		if (param.getInt("version", 0)!=Param.VERSION)
+		{
+			this.reset();
+		}
+		
+		
 		this.country = param.getInt("country", 79 );
 		this.city = param.getInt("city", 410);
 		
 		this.location = param.getString("location", "FR, Paris");
 		//lat = 50.7323f;  //507283
-		this.lat = param.getFloat("latitude", 50.7283f);
+		this.lat = param.getLong("latitude", 488779);
 		//lat = 50.7283f;
 		//lon = 3.104f;  //31616
-		this.lon = param.getFloat("longitude", 3.1616f);
+		this.lon = param.getLong("longitude", 23445);
 		//lon = 3.1616f;
-		this.alt = param.getFloat("alt", 0);
+		this.alt = param.getInt("alt", 0);
 		//alt = 0;
 		//gmt = TimeZone.getDefault().getRawOffset();
 		this.gmt = param.getInt("gmt", 100);
@@ -62,32 +73,50 @@ public class Settings {
 		//method = com.alpha.pt.PrayerTime.MUSLIM_LEAGUE;
 	}
 
+	public void reset()
+	{
+		 SharedPreferences param = this.context.getSharedPreferences(Param.NAME, 0);
+		 param.edit().clear().commit(); 	
+	}
+
 	public int getGmt() {
-		return gmt;
+		return gmt/100;
 	}
 	
 	public int getGmt2() {
-		return gmt/100;
+		return (int)gmt/100;
 	}
+	
+	public String getGmtString() {
+		return utils.time2str(gmt);
+	}
+	
 	public double getLat() {
-		return lat;
+		return lat/10000;
 	}
 	
 	public double getLon() {
-		return lon;
+		return lon/10000;
+	}
+	
+	public String getLatString() {
+		return ""+(int)(lat/10000)+"°"+(Math.abs(lat)%10000);
+	}
+	public String getLonString() {
+		return ""+(int)(lon/10000)+"°"+(Math.abs(lon)%10000);
 	}	
 	
 	public String getAngleQibla()
 	{
-		
-      double rlng =  utils.deg2rad(qibla_Lon - lon);
-      return utils.deg2str(utils.rad2deg(Math.atan2(Math.sin(rlng), Math.cos(utils.deg2rad(lat))* Math.tan( utils.deg2rad(qibla_Lat))- Math.sin(utils.deg2rad(lat))* Math.cos(rlng))));		
+		return utils.dms2str(this.getDegQibla());
+      //double rlng =  utils.deg2rad(qibla_Lon - this.getLon());
+      //return utils.deg2str(utils.rad2deg(Math.atan2(Math.sin(rlng), Math.cos(utils.deg2rad(this.getLat()))* Math.tan( utils.deg2rad(qibla_Lat))- Math.sin(utils.deg2rad(this.getLat()))* Math.cos(rlng))));		
 	}
 
 	public float getDegQibla()
 	{		
-      double rlng =  utils.deg2rad(qibla_Lon - lon);
-      return utils.rad2deg(Math.atan2(Math.sin(rlng), Math.cos(utils.deg2rad(lat))* Math.tan( utils.deg2rad(qibla_Lat))- Math.sin(utils.deg2rad(lat))* Math.cos(rlng)));		
+      double rlng =  utils.deg2rad(qibla_Lon - this.getLon());
+      return utils.rad2deg(Math.atan2(Math.sin(rlng), Math.cos(utils.deg2rad(this.getLat()))* Math.tan( utils.deg2rad(qibla_Lat))- Math.sin(utils.deg2rad(this.getLat()))* Math.cos(rlng)));		
 	}
 
 	/*
@@ -96,18 +125,19 @@ public class Settings {
 	}*/
 
 	public void save() {
-   SharedPreferences param = this.context.getSharedPreferences(PARAM_NAME, 0);
+   SharedPreferences param = this.context.getSharedPreferences(Param.NAME, 0);
       SharedPreferences.Editor editor = param.edit();
       editor.putString("location", this.location);
       editor.putInt("city", this.city);
       editor.putInt("country", this.country);      
-      editor.putFloat("latitude", (float)this.lat);
-      editor.putFloat("longitude", (float)this.lon);
-      editor.putFloat("alt", (float)this.alt);
+      editor.putLong("latitude", this.lat);
+      editor.putLong("longitude", this.lon);
+      editor.putInt("alt", this.alt);
       editor.putInt("gmt", this.gmt);
       editor.putFloat("angle", this.angle);
       editor.putInt("dst", this.dst);
       editor.putInt("method", this.method);
+      editor.putInt("version", Param.VERSION);
       editor.commit();
 
       Log.d("sam","location:"+this.location);
@@ -129,21 +159,10 @@ public class Settings {
 	}
 
 	public void setPosGps(double lat,double lon,double alt) {
-		this.lat = lat;
-		this.lon = lon;
-		this.alt = alt;
-		this.location = lat+"/"+lon+" (GPS)"; 
-	}
-	
-	public void setCity(String location,int country,int city,double lat,double lon,int gmt,int dst) {
-		this.lat = lat/10000;
-		this.lon = lon/10000;
-		this.gmt = gmt;
-		this.dst = dst;
-
-		this.country = country;
-		this.city = city;
-		this.location = location; 
+		this.lat = (int)(lat*10000);
+		this.lon = (int)(lon*10000);
+		this.alt = (int)alt;
+		this.location = this.getLatString()+"/"+this.getLonString()+" (GPS)"; 
 	}
 
 	public int getMethod() {
@@ -197,5 +216,31 @@ public class Settings {
 	public int getCity()
 	{
 		return this.city;
+	}
+
+	public void setCity(Activity act,int country, int city)
+	{
+		this.city = city;
+		this.country = country;
+		this.location ="-";
+		
+	    CityDB cb = new CityDB(context);
+	 	 	SQLiteDatabase db =  cb.getReadableDatabase();
+		   if (db!=null)
+			{
+				Cursor c = db.query("city", new String[]{"_id","ville","region","latitude","longitude","gmt","dst"}, "_id="+city+" and idpays="+country, null, null, null, null);
+				act.startManagingCursor(c);
+				String region;
+				while (c.moveToNext())
+				{
+					this.lat = c.getInt(3);
+					this.lon = c.getInt(4);
+					this.gmt = c.getInt(5);
+					this.dst = c.getInt(6);
+					this.location = c.getString(1);
+				}
+				 db.close();
+			}
+		
 	}
 }
